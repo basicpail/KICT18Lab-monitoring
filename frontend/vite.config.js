677e92e8b838b98e313540792f9609afa17597ba
SSG <basicpail93@gmail.com>
2024-08-10 14:44:00 +0900

    modify the initialState of dashBoardGraphData in deviceSlice.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ import eslint from 'vite-plugin-eslint' //에러 검사를 위한 eslint 추가
 export default defineConfig({
   plugins: [react(), eslint()],
   server: {
-    host: '192.168.0.100', // 원하는 IP 주소로 변경하세요
+    // host: '192.168.0.100`', // 원하는 IP 주소로 변경하세요
+    host: 'localhost', // 원하는 IP 주소로 변경하세요
     port: 5174 // 원하는 포트로 변경하세요
   },
   define: {
